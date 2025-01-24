b0803efae9d944212c02864f503302407fac222c
Alfonso Fuentes <fuentesw3008@gmail.com>
2025-01-23 23:02:37 -0500

    DockerCompose

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -22,7 +22,7 @@ const envsSchema = joi
 
 const { error, value } = envsSchema.validate({
   ...process.env,
-  NATS_SERVERS: process.env.NATS_SERVER?.split(','),
+  NATS_SERVERS: process.env.NATS_SERVERS?.split(','),
 });
 
 if (error) throw new Error(`Config validation error: ${error.message}`);
